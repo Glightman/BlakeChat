7ce0f4a3f3d5468c8413184ea46c44f71ea020cb
Gabriel Santana <gabriellima36716@gmail.com>
2021-09-14 22:49:38 -0300

    Programing the model fo the aplication

---
+++ BlakeChat/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlakeChat.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public List<Message> messages { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ BlakeChat/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlakeChat.Models
+{
+    public class Message
+    {
+        public int id { get; set; }
+        public string userName { get; set; }
+        [Required]
+        public string text { get; set; }
+        public DateTime DateTime { get; set; }
+        public string userId { get; set; }
+        public AppUser appUser { get; set; }  
+    }
+}
