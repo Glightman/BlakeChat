fb817437b614b102e61fcd959b27518a79275117
Gabriel Santana <gabriellima36716@gmail.com>
2021-09-15 21:50:51 -0300

    Creating DbContext

--- BlakeChat/Data/Context.cs
+++ BlakeChat/Data/Context.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BlakeChat.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,15 @@ namespace BlakeChat.Data
 {
     public class Context : IdentityDbContext
     {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Message>()
+                .HasOne<AppUser>(a => a.appUser)
+                .WithMany(d => d.messages)
+                .HasForeignKey(d => d.userId);
+        }
+        public DbSet<Message> Message { get; set; }
     }
 }
